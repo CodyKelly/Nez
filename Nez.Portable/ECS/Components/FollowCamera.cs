612c092fa9d53f903a04714340824fafeea4b92d
Cody Kelly <cody.kelly@ucdenver.edu>
2020-04-09 13:10:36 -0600

    Added a function to recenter camera on target to FollowCamera component

--- Nez.Portable/ECS/Components/FollowCamera.cs
+++ Nez.Portable/ECS/Components/FollowCamera.cs
@@ -190,6 +190,12 @@ namespace Nez
 		{
 			_targetEntity = targetEntity;
 			_cameraStyle = cameraStyle;
+
+			CenterOnTarget();
+		}
+
+		public void CenterOnTarget()
+		{
 			var cameraBounds = Camera.Bounds;
 
 			switch (_cameraStyle)
